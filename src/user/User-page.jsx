d192e3698795f93087ef4523f9971d4a22d748b9
Anaelica <anaanaelica@gmail.com>
2026-06-17 11:41:49 -0300

    Mudança do array pro componente pai e adição de pesquisa no componente search

--- src/user/User-page.jsx
+++ src/user/User-page.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import CardUsuarios from "./cards/Users-Cards.jsx";
-import Search from "./search/search.jsx";
+import Search from "./search/search.jsx"; 
 
 export default function UserPage() {
+  const [search, setSearch] = useState("");
+  
+  const usuarios = [
+      { 
+        avatar: "https://i.pinimg.com/1200x/c3/b6/27/c3b62766f107dd52da4de3ed53976cbe.jpg",
+        name: "anaelica barbosa", 
+        email: "anaanaelica@gmail"
+      },
+      { 
+        avatar: "https://i.pinimg.com/736x/66/86/ae/6686ae04340f0125502a1fc08bf482da.jpg",
+        name: "Eduardo", 
+        email: "eduaaaaaaa@gmail"
+      }
+    ]
   return (
     <div className="flex flex-col gap-16 items-center justify-center">
-      <Search />
-      <CardUsuarios />
+      <Search search={search} setSearch={setSearch}/>
+      <CardUsuarios usuarios={usuarios} search={search}/>
     </div>
   );
 }
