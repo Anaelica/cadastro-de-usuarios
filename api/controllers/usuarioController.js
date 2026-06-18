2d6a76539465531f79834739e504be267538bbbf
Anaelica <anaanaelica@gmail.com>
2026-06-18 14:08:42 -0300

    adicionação da rota de atualização de usuários

--- api/controllers/usuarioController.js
+++ api/controllers/usuarioController.js
@@ -51,3 +51,54 @@ export async function criarUsuario(request, response) {
     });
   }
 }
+
+export async function atualizarUsuario(request, response) {
+  try {
+    const { id } = request.params
+    const { name, email } = request.body
+    
+    //Montando os dados que serão atualizados
+    const data = {
+      name, 
+      email,
+    }
+
+    //Caso mandem uma nova foto ela substitui a antiga
+    if(request.file) {
+      data.avatar = `http://localhost:3001/uploads/${request.file.filename}`
+    }
+
+      const usuarioAtualizado = await prisma.usuario.update({
+        where: {
+          id: Number(id),
+        },
+        data,
+      });
+
+return response.status(200).json(usuarioAtualizado);
+      
+      return response.status(200).json(usuarioAtualizado)
+  } catch (error) {
+    console.error(error)
+    console.log(Prisma);
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2025"
+    ) {
+      return response.status(404).json({
+        message: "Usuario não encontrado.",
+      })
+    }
+
+    if(
+      error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2002"
+    ){
+      return response.status(409).json({
+        message: "Já existe um usuario com esse e-mail."
+      })
+    }
+
+    return response.status(500).json({
+      message: "Erro ao atualizar usuario."
+    })
+  }
+}
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -5,11 +5,18 @@ import upload from "../middleware/upload.js";
 import {
   listarUsuarios,
   criarUsuario,
+  atualizarUsuario,
 } from "../controllers/usuarioController.js";
 
 const router = Router();
 
 router.get("/", listarUsuarios);
+
+router.put(
+  "/:id",
+  upload.single("avatar"),
+  atualizarUsuario
+);
 
 router.post(
   "/",
