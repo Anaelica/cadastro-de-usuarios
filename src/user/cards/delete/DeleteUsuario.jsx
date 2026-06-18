9e1d2aa5f5e36a47d242baa63d635b6486bf7ca0
Anaelica <anaanaelica@gmail.com>
2026-06-17 21:06:18 -0300

    Modal pra exclusão de um usuario

--- src/user/cards/delete/DeleteUsuario.jsx
+++ src/user/cards/delete/DeleteUsuario.jsx
@@ -15,7 +15,7 @@ export default function DeletarUsuario({ usuario, onClose }) {
         <div className="flex gap-8">
           <button
             onClick={() => {
-              console.log(usuario.id); // aqui você faz o delete
+              console.log(usuario.id); 
               onClose();
             }}
             className="mt-4 bg-red-700 px-4 py-2 rounded-lg text-white"
