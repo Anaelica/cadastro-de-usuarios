9f6ba88d2672896324169a5b8e88bfb3391c1211
Anaelica <anaanaelica@gmail.com>
2026-06-18 09:20:07 -0300

    implementação da rota POST /usuarios

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const prisma = new PrismaClient()
 app.use(cors())
 app.use(express.json())
 
-      //Criação da rota get e tratamento de excessões
+    //Criação da rota get e tratamento de excessões
 app.get("/usuarios", async (request, response) => {
   try {
     const usuarios = await prisma.usuario.findMany();
@@ -25,10 +25,31 @@ app.get("/usuarios", async (request, response) => {
       erro: "Erro ao buscar usuario"
     })
   } 
+})
+    //Criação da rota post pra criar um novo usuario 
+app.post("/usuarios", async (request, response) => {
+  //tratamento de excessão caso a requisição dê erro
+  try {
+    const { name, email, avatar } =  request.body
 
-  return response.json({
-    mensage: "Listagem de usuarios"
-  })
+      //o Prisma executa e salva no postgres
+    const usuario = await prisma.usuario.create({
+      //Depois devolve um usuario criado
+      data: {
+        name,
+        email,
+        avatar
+      },
+    })
+
+    response.status(200).json(usuario)
+  } catch (error) {
+    console.error(error)
+
+    response.status(500).json({
+      mensage: "Erro ao criar um novo usuario."
+    })
+  }
 })
 
 app.listen(3001, () => {
