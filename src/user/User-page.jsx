5442d13cefde0ac603d5822d9a5ddfdefa98d6d3
Anaelica <anaanaelica@gmail.com>
2026-06-18 09:28:26 -0300

    integrar frontend com API de usuários

--- src/user/User-page.jsx
+++ src/user/User-page.jsx
@@ -1,32 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CardUsuarios from "./cards/Users-Cards.jsx";
 import Search from "./search/search.jsx"; 
 import CriarUsuario from "./criar-usuario/CriarUsuario.jsx";
 
 export default function UserPage() {
   const [search, setSearch] = useState("");
+  const [usuarios, setUsuarios] = useState([])
+
+  useEffect(() => {
+    buscarUsuarios();
+  }, [])
+
+  async function buscarUsuarios(){
+    try {
+      const response = await fetch("http://localhost:3001/usuarios")
+
+      const data = await response.json()
+      setUsuarios(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
   
-  const usuarios = [
-      { 
-        id: 1,
-        avatar: "https://i.pinimg.com/1200x/c3/b6/27/c3b62766f107dd52da4de3ed53976cbe.jpg",
-        name: "anaelica barbosa", 
-        email: "anaanaelica@gmail"
-      },
-      { 
-        id: 2,
-        avatar: "https://i.pinimg.com/736x/66/86/ae/6686ae04340f0125502a1fc08bf482da.jpg",
-        name: "Eduardo", 
-        email: "eduaaaaaaa@gmail",
-      }
-    ]
   return (
     <div className="flex flex-col gap-10 items-center justify-center">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center justify-center mt-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-4">
         <Search search={search} setSearch={setSearch}/>
         <CriarUsuario />
       </div>
-      <CardUsuarios usuarios={usuarios} search={search}/>
+      <CardUsuarios 
+      usuarios={usuarios} 
+      search={search}/>
     </div>
   );
 }
